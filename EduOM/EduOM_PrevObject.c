3665cac0e846db0bce7786c8a7155da922e263de
joon363 <joon363@naver.com>
2025-04-09 18:26:45 +0900

    Implemented EduOM_PrevObject

--- EduOM/EduOM_PrevObject.c
+++ EduOM/EduOM_PrevObject.c
@@ -72,11 +72,14 @@ Four EduOM_PrevObject(
 {
     Four e;			/* error */
     Two  i;			/* index */
+    Two  firstObjIdx;			/* index of first object */
     Four offset;		/* starting offset of object within a page */
     PageID pid;			/* a page identifier */
+    VolNo volNo;			/* a temporary var for volNo */
     PageNo pageNo;		/* a temporary var for previous page's PageNo */
     SlottedPage *apage;		/* a pointer to the data page */
     Object *obj;		/* a pointer to the Object */
+    PhysicalFileID pFid;	/* file in which the objects are located */
     SlottedPage *catPage;	/* buffer page containing the catalog object */
     sm_CatOverlayForData *catEntry; /* overlay structure for catalog object access */
 
@@ -87,7 +90,141 @@ Four EduOM_PrevObject(
     
     if (prevOID == NULL) ERR(eBADOBJECTID_OM);
 
-    
+    /* 현재object의 이전 object의 ID를 반환함
+     * 파라미터로주어진curOID가 NULL 인 경우,
+     * – File의 마지막 page의 slot array 상에서의 마지막 object의 ID를 반환함
+     * 파라미터로주어진curOID가 NULL 이 아닌경우,
+     * – curOID에 대응하는 object를 탐색함
+     * – Slot array 상에서, 탐색한 object의 이전 object의 ID를 반환함
+     *   » 탐색한object가 page의 첫 번째 object인 경우,
+     *    • 이전page의마지막object의 ID를 반환함
+     *   » 탐색한object가 file의 첫 번째 page의 첫 번째 object인 경우,
+     *   • EOS (End Of Scan) 를 반환함*/
+
+    // Get sm_CatOverlayForData from File
+    volNo = catObjForFile->volNo;
+    pageNo = catObjForFile->pageNo;
+    MAKE_PHYSICALFILEID(pFid, volNo, pageNo);
+    e = BfM_GetTrain(&pFid, &catPage, PAGE_BUF);
+    if(e < eNOERROR) ERR(e);
+    GET_PTR_TO_CATENTRY_FOR_DATA(catObjForFile, catPage, catEntry);
+
+    // 파라미터로 주어진 curOID가 NULL인 경우
+    if (curOID == NULL) {
+        // File의 마지막 page의 slot array 상에서의 마지막 object의 ID를 반환함
+        // 마지막 page
+        volNo = catObjForFile->volNo;
+        pageNo = catEntry->lastPage;
+        MAKE_PAGEID(pid, volNo, pageNo);
+        e = BfM_GetTrain(&pid, &apage, PAGE_BUF);
+        if (e < eNOERROR) ERRB1(e, &pFid, PAGE_BUF);
+
+        // 마지막 object
+        /* OUT the previous object of a current object */
+        volNo = apage->header.pid.volNo;
+        pageNo = apage->header.pid.pageNo;
+        MAKE_OBJECTID(*prevOID, volNo, pageNo, i, apage->slot[-i].unique);
+
+        /* OUT the object header of previous object */
+        i = apage->header.nSlots - 1;
+        offset = apage->slot[-i].offset;
+        obj = &(apage->data[offset]);
+        objHdr = &obj->header; 
+        
+        e = BfM_FreeTrain(&pFid, PAGE_BUF);
+        if (e < eNOERROR) ERR(e);
+        e = BfM_FreeTrain(&pid, PAGE_BUF);
+        if (e < eNOERROR) ERR(e);
+
+        return(eNOERROR);
+    }
+    /* 파라미터로주어진curOID가 NULL 이 아닌경우,
+     * – curOID에 대응하는 object를 탐색함
+     * – Slot array 상에서, 탐색한 object의 이전 object의 ID를 반환함
+     *   » 탐색한object가 page의 첫 번째 object인 경우,
+     *    • 이전page의마지막object의 ID를 반환함
+     *   » 탐색한object가 file의 첫 번째 page의 첫 번째 object인 경우,
+     *   • EOS (End Of Scan) 를 반환함*/
+    else {
+        volNo = curOID->volNo;
+        pageNo = curOID->pageNo;
+        MAKE_PAGEID(pid, volNo, pageNo);
+        e = BfM_GetTrain(&pid, &apage, PAGE_BUF);
+        if (e < eNOERROR) ERRB1(e, &pFid, PAGE_BUF);
+
+        // index of first object
+        for (firstObjIdx = 0; firstObjIdx < apage->header.nSlots; firstObjIdx++) {
+            if (apage->slot[-firstObjIdx].offset != EMPTYSLOT) break;
+        }
+
+        // Slot array 상에서, 탐색한 object의 이전 object의 ID를 반환함
+        // 탐색한 object가 page의 첫 번째 object인 경우
+        if (curOID->slotNo==firstObjIdx) {
+            // file의 첫번째 page인 경우
+            if (catEntry->firstPage == apage->header.pid.pageNo) {
+                e = BfM_FreeTrain(&pFid, PAGE_BUF);
+                if (e < eNOERROR) ERR(e);
+                e = BfM_FreeTrain(&pid, PAGE_BUF);
+                if (e < eNOERROR) ERR(e);
+
+                return(EOS);
+            }
+            // file의 첫번째 page가 아닌 경우
+            else {
+                // curOID->pageNo 페이지 해제
+                e = BfM_FreeTrain(&pid, PAGE_BUF);
+                if (e < eNOERROR) ERR(e);
+
+                // 이전 page의 마지막 object의 ID를 반환함
+                volNo = curOID->volNo;
+                pageNo = apage->header.prevPage;
+                MAKE_PAGEID(pid, volNo, pageNo);
+                e = BfM_GetTrain(&pid, &apage, PAGE_BUF);
+                if (e < eNOERROR) ERRB1(e, &pFid, PAGE_BUF);
+                
+                /* OUT the next Object of a current Object */
+                volNo = apage->header.pid.volNo;
+                pageNo = apage->header.pid.pageNo;
+                MAKE_OBJECTID(*prevOID, volNo, pageNo, i, apage->slot[-i].unique);
+        
+                /* OUT the object header of next object */
+                i = apage->header.nSlots - 1;
+                offset = apage->slot[-i].offset;
+                obj = &(apage->data[offset]);
+                objHdr = &obj->header; 
+                
+                e = BfM_FreeTrain(&pFid, PAGE_BUF);
+                if (e < eNOERROR) ERR(e);
+                e = BfM_FreeTrain(&pid, PAGE_BUF);
+                if (e < eNOERROR) ERR(e);
+
+                return(eNOERROR);
+            }
+        }
+        // 탐색한 object가 page의 첫번째 object가 아닌 경우,
+        else {
+            for (i = curOID->slotNo - 1; i >= 0; i--) {
+                if (apage->slot[-i].offset != EMPTYSLOT) {
+                    /* OUT the next Object of a current Object */
+                    volNo = apage->header.pid.volNo;
+                    pageNo = apage->header.pid.pageNo;
+                    MAKE_OBJECTID(*prevOID, volNo, pageNo, i, apage->slot[i].unique);
+            
+                    /* OUT the object header of next object */
+                    offset = apage->slot[-i].offset;
+                    obj = &(apage->data[offset]);
+                    objHdr = &obj->header; 
+                    
+                    e = BfM_FreeTrain(&pFid, PAGE_BUF);
+                    if (e < eNOERROR) ERR(e);
+                    e = BfM_FreeTrain(&pid, PAGE_BUF);
+                    if (e < eNOERROR) ERR(e);
+
+                    return(eNOERROR);
+                }
+            }
+        }
+    }
 
     return(EOS);
     
